88b8299dd093ff54b0e3dad7167b997e2c336d3d
Joseph Rousoss <jroussos01@gmail.com>
2022-07-15 20:13:40 -0400

    railgun with animations

--- Dice_GameJam_Submission/Assets/Scripts/Guns/FireRailGun.cs
+++ Dice_GameJam_Submission/Assets/Scripts/Guns/FireRailGun.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class FireRailGun : MonoBehaviour
 {
+    BoxCollider2D gunCol;
+    SpriteRenderer myrend;
+    Animator myanim;
+
+    private bool mayFire = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myrend = GetComponent<SpriteRenderer>();
+        myrend.enabled = false;
+        myanim = GetComponent<Animator>();
+        gunCol = GetComponent<BoxCollider2D>();
+        gunCol.enabled = false;
     }
 
     // Update is called once per frame
@@ -18,9 +27,34 @@ public class FireRailGun : MonoBehaviour
 
     private void FireGun()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && mayFire)
         {
-            print("fire!");
+            mayFire = false;
+            StartCoroutine(ChargeUptimer());
         }
+    }
+
+    IEnumerator ChargeUptimer()
+    {
+        print("charging!");
+        yield return new WaitForSeconds(0.5f);
+        gunCol.enabled = true;
+        myrend.enabled = true;
+        myanim.SetBool("RailGunFire", true);
+        StartCoroutine(ShotDuration());
+        StartCoroutine(FireRepeatTimer());
+    }
+    IEnumerator ShotDuration()
+    {
+        yield return new WaitForSeconds(0.5f);
+        gunCol.enabled = false;
+        myrend.enabled = false;
+        myanim.SetBool("RailGunFire", false);
+    }
+
+   IEnumerator FireRepeatTimer()
+    {
+        yield return new WaitForSeconds(1f);
+        mayFire = true;
     }
 }
