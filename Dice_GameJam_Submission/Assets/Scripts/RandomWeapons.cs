9275eec4b2d5ce6051562f140ece5e64773e162c
Joseph Rousoss <jroussos01@gmail.com>
2022-07-16 13:22:27 -0400

    better sdkljdsklfsd

--- Dice_GameJam_Submission/Assets/Scripts/RandomWeapons.cs
+++ Dice_GameJam_Submission/Assets/Scripts/RandomWeapons.cs
@@ -13,7 +13,9 @@ public class RandomWeapons : MonoBehaviour
     private int prevWeapon = 0;
     private float timer = 0f;
     private bool maySwap = true;
+    private bool swappingWeapons = false;
 
+    private float waitTime = 3f;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,96 +27,96 @@ public class RandomWeapons : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        SetWeapon();
-    }
-
-    private void SetWeapon()
-    {
-        timer += Time.deltaTime;
-        if (timer >= 5f)
+        if (maySwap)
         {
-            prevWeapon = randomNum;
-            randomNum = Random.Range(0, 3);
-            while (randomNum == prevWeapon)
-            {
-                randomNum = Random.Range(0, 3);
-            }
-            maySwap = true;
-            timer = 0f;
+            print("swapping");
+            maySwap = false;
+            StartCoroutine(WeaponTimer());
         }
-
-        switch (randomNum)
-        {
-            case 0:
-                if (maySwap)
-                {
-                    StartCoroutine(EnableMachineGun());
-                    maySwap = false;
-                }
-                break;
-
-            case 1:
-                if (maySwap)
-                {
-                    StartCoroutine(EnableRailGun());
-                    maySwap = false;
-                }
-                break;
-
-            case 2:
-                if (maySwap)
-                {
-                    StartCoroutine(EnableFireBall());
-                    maySwap = false;
-                }
-                break;
-
-            default:
-                if (maySwap)
-                {
-                    StartCoroutine(EnableMachineGun());
-                    maySwap = false;
-                }
-                break;
-
-        }
+        //SetWeapon();
     }
 
     // use the coroutines to start off the animations
-    public int GetRandomInt()
-    {
-        return randomNum;
-    }
     IEnumerator EnableMachineGun()
     {
-        yield return new WaitForSeconds(5f);
+        yield return new WaitForSeconds(waitTime);
         MachineGun.SetActive(true);
         RailGun.SetActive(false);
         FireBall.SetActive(false);
         FindObjectOfType<FireMachineGun>().Set_mayFire(true);
         FindObjectOfType<WeaponsUI>().SetMachineGun();
+        maySwap = true;
     }
 
     IEnumerator EnableRailGun()
     {
-        yield return new WaitForSeconds(5f);
+        yield return new WaitForSeconds(waitTime);
         MachineGun.SetActive(false);
         RailGun.SetActive(true);
         FireBall.SetActive(false);
         FindObjectOfType<FireRailGun>().Set_mayFire(true);
         FindObjectOfType<WeaponsUI>().SetRailGun();
+        //timer = 0f;
+        //swappingWeapons = false;
+        maySwap = true;
     }
 
     IEnumerator EnableFireBall()
     {
-        yield return new WaitForSeconds(5f);
+        yield return new WaitForSeconds(waitTime);
         MachineGun.SetActive(false);
         RailGun.SetActive(false);
         FireBall.SetActive(true);
         FindObjectOfType<Shoot_FireBall>().Set_mayFire(true);
         FindObjectOfType<WeaponsUI>().SetFireBall();
+        //timer = 0f;
+        //swappingWeapons = false;
+        maySwap = true;
     }
 
-    // Animations
+    // Animations will be put in later?
+
+    private void SwapWeapons()
+    {
+        switch (GetRandomNum())
+        {
+            case 0:
+                StartCoroutine(EnableMachineGun()); 
+                break;
+
+            case 1:
+                StartCoroutine(EnableRailGun());
+                break;
+
+            case 2:
+                StartCoroutine(EnableFireBall());
+                break;
+
+            default:
+                StartCoroutine(EnableMachineGun());
+                break;
+
+        }
+    }
+
+
+    IEnumerator WeaponTimer()
+    {
+        yield return new WaitForSeconds(waitTime);
+        SwapWeapons();
+    }
+
+    private int GetRandomNum()
+    {
+        prevWeapon = randomNum;
+        randomNum = Random.Range(0, 3);
+        while (randomNum == prevWeapon)
+        {
+            randomNum = Random.Range(0, 3);
+        }
+
+        return randomNum;
+    }
+
 
 }
